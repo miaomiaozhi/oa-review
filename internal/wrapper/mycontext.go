298be10a64ced0ac87f60db4b0ed34fcd8f6293c
mozezhao <mozezhao@moresec.cn>
2023-06-02 13:53:51 +0800

    finish

--- internal/wrapper/mycontext.go
+++ internal/wrapper/mycontext.go
@@ -18,8 +18,8 @@ type Context struct {
 
 type AuthResult struct {
 	Token    string `json:"Token"`    // token
-	UserID   int64  `json:"UserId"`   //用户id
-	UserName string `json:"UserName"` //用户名
+	UserID   int64  `json:"UserId"`   // 用户id
+	UserName string `json:"UserName"` // 用户名
 	Priority int64  `json:"Priority"` // 优先级
 }
 
